5ff541077175169bd603808ad94b3f5a6dda8518
Chen Haotian <chtunsw@gmail.com>
2019-02-19 22:24:30 +1100

    mockdata updated

--- src/mockdata/shift.js
+++ src/mockdata/shift.js
@@ -1,19 +1,32 @@
+import Putin_portrait from '../images/portraits/Putin.jpg'
+import Tom_portrait from '../images/portraits/Tom.jpg'
+import LingLing_portrait from '../images/portraits/LingLing.jpg'
+
 const shift_1 = {
+    paople_id:'0001',
+    name:'Putin',
+    portrait: Putin_portrait,
     title:'admin',
     start_time:'9:00am',
     end_time:'5:30pm'
 }
 
 const shift_2 = {
+    people_id:'0002',
+    name:'Tom',
+    portrait: Tom_portrait,
     title:'leader',
-    start_time:'9:00am',
-    end_time:'5:30pm'
+    start_time:'8:00am',
+    end_time:'4:30pm'
 }
 
 const shift_3 = {
+    people_id:'0003',
+    name:'LingLing',
+    portrait: LingLing_portrait,
     title:'staff',
-    start_time:'9:00am',
-    end_time:'5:30pm'
+    start_time:'10:00am',
+    end_time:'6:30pm'
 }
 
 export {shift_1, shift_2, shift_3}
--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { people_1 } from '../mockdata/people'
+import { people_1, people_2, people_3 } from '../mockdata/people'
 import { shift_1, shift_2, shift_3 } from '../mockdata/shift'
 
 import Table_PeopleBlock from '../components/Table_PeopleBlock'
@@ -12,6 +12,8 @@ class Setting extends Component {
         <div >
         Setting
         <Table_PeopleBlock people={people_1} />
+        <Table_PeopleBlock people={people_2} />
+        <Table_PeopleBlock people={people_3} />
         <Table_ShiftBlock shift={null} />
         <Table_ShiftBlock shift={shift_1} />
         <Table_ShiftBlock shift={shift_2} />
